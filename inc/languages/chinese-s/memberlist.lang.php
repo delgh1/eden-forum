929bc929c0a644a841b9179a1cf82727941287a9
Jing Luo <szmun.luoj@gmail.com>
2023-04-26 21:13:35 +0900

    Add misc.lang.php, memberlist.lang.php
    Signed-off-by: Jing Luo <szmun.luoj@gmail.com>

---
+++ inc/languages/chinese-s/memberlist.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * MyBB 1.8 Chinese (Simplified) Language Pack
+ * https://github.com/delgh1/mybb-chinese-translation
+ * Maintainer: Jing Luo
+ * Apr 26, 2023
+ */
+
+$l['nav_memberlist'] = "成员列表";
+
+$l['memberlist_disabled'] = "成员列表功能已被管理员禁用。";
+
+$l['member_list'] = "成员列表";
+$l['avatar']  = "头像";
+$l['username'] = "用户名";
+$l['joined'] = "加入";
+$l['lastvisit'] = "上次访问";
+$l['posts'] = "帖子数";
+$l['threads'] = "主题数";
+$l['referrals'] = "介绍";
+$l['search_members'] = "搜索成员列表";
+$l['website'] = "网站";
+$l['sort_by'] = "排序";
+$l['contains'] = "包含：";
+$l['exactly'] = "刚刚好:";
+$l['sort_by_username'] = "以用户名排序";
+$l['sort_by_regdate'] = "以注册日期排序";
+$l['sort_by_lastvisit'] = "以上次访问时间排序";
+$l['sort_by_posts'] = "以帖子数排序";
+$l['sort_by_threads'] = "以主题数排序";
+$l['sort_by_referrals'] = "以介绍的成员排序";
+$l['order_asc'] = "升序";
+$l['order_desc'] = "降序";
+$l['asc'] = "升";
+$l['desc'] = "降";
+$l['forumteam'] = "显示论坛团队";
+$l['advanced_search'] = "高级搜索";
+$l['nav_memberlist_search'] = "搜索";
+$l['search_member_list'] = "搜索成员列表";
+$l['search_criteria'] = "搜索条件";
+$l['begins_with'] = "开始于";
+$l['username_contains'] = "包括";
+$l['username_exact'] = "刚好";
+$l['search_website'] = "网站URL";
+$l['search_skype'] = "Skype地址";
+$l['search_google'] = "Google Hangouts地址";
+$l['search_icq'] = "ICQ号码";
+$l['search_options'] = "搜索选项";
+$l['per_page'] = "每页结果";
+$l['search'] = "搜索";
+
+$l['error_no_members'] = "<p>以你输入的搜索条件没有找到成员。</p><p>请输入一个不同的关键词再试。</p>";
+
+$l['a'] = 'A';
+$l['b'] = 'B';
+$l['c'] = 'C';
+$l['d'] = 'D';
+$l['e'] = 'E';
+$l['f'] = 'F';
+$l['g'] = 'G';
+$l['h'] = 'H';
+$l['i'] = 'I';
+$l['j'] = 'J';
+$l['k'] = 'K';
+$l['l'] = 'L';
+$l['m'] = 'M';
+$l['n'] = 'N';
+$l['o'] = 'O';
+$l['p'] = 'P';
+$l['q'] = 'Q';
+$l['r'] = 'R';
+$l['s'] = 'S';
+$l['t'] = 'T';
+$l['u'] = 'U';
+$l['v'] = 'V';
+$l['w'] = 'W';
+$l['x'] = 'X';
+$l['y'] = 'Y';
+$l['z'] = 'Z';
+
---
+++ inc/languages/chinese-s/misc.lang.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * MyBB 1.8 Chinese (Simplified) Language Pack
+ * https://github.com/delgh1/mybb-chinese-translation
+ * Maintainer: Jing Luo
+ * Apr 26, 2023
+ */
+
+$l['nav_helpdocs'] = "帮助文档";
+$l['nav_smilies'] = "表情列表";
+$l['nav_syndication'] = "最新的RSS订阅";
+
+$l['buddy_list'] = "好友列表";
+$l['online'] = "在线";
+$l['online_none'] = "<em>你没有在线的好友</em>";
+$l['offline'] = "离线";
+$l['offline_none'] = "<em>你没有离线的好友</em>";
+$l['delete_buddy'] = "X";
+$l['pm_buddy'] = "发送私信";
+$l['last_active'] = "<strong>最近活跃：</strong> {1}";
+$l['close'] = "关闭";
+$l['no_buddies'] = "<em>你的好友列表是空的。用你的用户控制面板或者访问一个用户的个人资料来添加好友。</em>";
+
+$l['help_docs'] = "帮助文档";
+
+$l['search_help_documents'] = "搜索帮助文档";
+$l['search_by_name'] = "按名字搜索";
+$l['search_by_document'] = "按文档搜索";
+$l['enter_keywords'] = "输入关键字";
+$l['search'] = "搜索";
+$l['redirect_searchresults'] = "谢谢你，你的所有已经被提交，现在你会被带到结果列表。";
+$l['search_results'] = "搜索结果";
+$l['help_doc_results'] = "帮助文档结果";
+$l['document'] = "文档";
+$l['error_nosearchresults'] = "对不起，使用你提供的查询信息没有返回结果。请重新设置你的检索条件再试。";
+$l['no_help_results'] = "对不起，使用你提供的查询信息没有返回结果。";
+$l['error_helpsearchdisabled'] = "搜索帮助文档的功能已被管理员禁用。";
+
+$l['smilies_listing'] = "表情列表";
+$l['name'] = "名字";
+$l['abbreviation'] = "简写";
+$l['click_to_add'] = "点击一个表情来把它插入到消息内容";
+$l['close_window'] = "关闭窗口";
+$l['no_smilies'] = "现在没有表情可用。";
+
+$l['who_posted'] = "谁发帖了？";
+$l['total_posts'] = "总帖数：";
+$l['user'] = "用户";
+$l['num_posts'] = "帖子数";
+
+$l['forum_rules'] = "{1} - 规则";
+
+$l['error_invalid_limit'] = "你输入的时间线项目限制无效。请指定一个有效的限制。";
+
+$l['syndication'] = "最新的RSS订阅";
+$l['syndication_generated_url'] = "你生成的RSS订阅URL:";
+$l['syndication_note'] = "在下面，你可以生成特定的RSS链接。链接可以按所有版块，每个版块，或者一定数量的版块生成。你将会得到一个链接，可以复制到RSS阅读器。 <i><a href=\"https://en.wikipedia.org/wiki/RSS\" target=\"_blank\" rel=\"noopener\">什么是RSS?</a></i>";
+$l['syndication_forum'] = "订阅的版块：";
+$l['syndication_forum_desc'] = "请从右边选择一个版块。按CTRL键选择多个版块。";
+$l['syndication_version'] = "时间线版本：";
+$l['syndication_version_desc'] = "请选择你想要生成的时间线的版本。";
+$l['syndication_version_json1'] = "JSON Feed 1";
+$l['syndication_version_atom1'] = "Atom 1.0";
+$l['syndication_version_rss2'] = "RSS 2.00 （默认）";
+$l['syndication_generate'] = "生成订阅URL";
+$l['syndication_limit'] = "限制：";
+$l['syndication_limit_desc'] = "每次下载的主题数。50是最大值。";
+$l['syndication_threads_time'] = "主题每次";
+$l['syndicate_all_forums'] = "订阅所有版块";
+
+$l['redirect_markforumread'] = "选择的版块已标记已读。";
+$l['redirect_markforumsread'] = "所有版块已标记已读。";
+$l['redirect_forumpasscleared'] = "本版块保存的密码已被清除。";
+$l['redirect_cookiescleared'] = "所有cookies已被清除。";
+
+$l['error_invalidforum'] = "无效版块";
+$l['error_invalidhelpdoc'] = "指定的帮助文档似乎不存在。";
+$l['error_invalidsearch'] = "指定了无效的搜索。请返回重试。";
+$l['error_no_search_support'] = "这个数据库引擎不支持搜索。";
+$l['error_searchflooding'] = "对不起，你只能每 {1} 秒搜索1次。请再等 {2} 秒再试。";
+$l['error_searchflooding_1'] = "对不起，你只能每 {1} 秒搜索1次。请再等 1 秒再试。";
+
+$l['dst_settings_updated'] = "你的夏令时设置已经被成功调整。<br /><br />你将会被带回到论坛主页。";
