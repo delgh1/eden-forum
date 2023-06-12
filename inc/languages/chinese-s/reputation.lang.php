8d2eb025591e3054ea0515ec8548f01071b9ef27
Jing Luo <szmun.luoj@gmail.com>
2023-06-12 18:26:17 +0900

    Add reputation.lang.php
    Signed-off-by: Jing Luo <szmun.luoj@gmail.com>

---
+++ inc/languages/chinese-s/reputation.lang.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * MyBB 1.8 Chinese (Simplified) Language Pack
+ * https://github.com/delgh1/mybb-chinese-translation
+ * Maintainer: Jing Luo
+ * June 12, 2023
+ */
+
+$l['nav_reputation'] = "声望报告";
+$l['nav_profile'] = "{1}的个人资料";
+$l['reputation_disabled'] = "你不能使用声望系统因为它已经被管理员禁用。";
+
+$l['reputation'] = "声望";
+$l['error'] = "错误";
+$l['add_no_uid'] = "你没有指定要给声望的用户。";
+$l['add_no_permission'] = "你没有权限给用户声望评分。";
+$l['add_disabled'] = "你不能给这个用户组的用户添加声望。";
+$l['add_yours'] = "你不能给自己添加声望。";
+$l['add_invalidpower'] = "你从列表中选择了无效的评分。";
+$l['add_maxperday'] = "你已经给了今天你被允许给的声望。";
+$l['add_maxperuser'] = "你已经给了这个用户今天你被允许给的声望。";
+$l['add_maxperthread'] = "你已经给了这个用户在这个主题今天你被允许给的声望。";
+$l['add_no_comment'] = "你必须输入一个评价，至少{1}字符，才能给这个用户一个声望评分。";
+$l['add_toolong'] = "你必须输入理由不长于{1}字符。";
+$l['add_negative_disabled'] = "管理员禁用了负面声望。";
+$l['add_neutral_disabled'] = "管理员禁用了中立声望。";
+$l['add_positive_disabled'] = "管理员禁用了正面声望。";
+$l['add_all_rep_disabled'] = "管理员禁用了所有声望。你不能评价这个用户";
+$l['reputation_report'] = "{1}的声望报告";
+$l['reputation_members'] = "成员的声望：";
+$l['reputation_posts'] = "帖子的声望：";
+$l['summary'] = "总结";
+$l['total_reputation'] = "声望共计";
+$l['post_reputation'] = "帖子声望";
+$l['positive_count'] = "正面";
+$l['neutral_count'] = "中立";
+$l['negative_count'] = "负面";
+$l['last_week'] = "上星期";
+$l['last_month'] = "上个月";
+$l['last_6months'] = "最近6个月";
+$l['all_time'] = "所有时间";
+$l['comments'] = "评论";
+$l['close_window'] = "关闭窗口";
+$l['add_reputation_vote'] = "给{1}添加声望评价";
+$l['add_reputation_to_post'] = "这个声望是给{1}的帖子<br />";
+$l['neg_rep_disabled'] = "<span class=\"smalltext\">* - <em>负面声望目前禁用</em></span>";
+$l['pos_rep_disabled'] = "<span class=\"smalltext\">* - <em>正面声望目前禁用</em></span>";
+$l['neu_rep_disabled'] = "<span class=\"smalltext\">* - <em>中立声望目前禁用</em></span>";
+$l['no_comment_needed'] = "你在添加声望因为这个用户的帖子，链接到他们的个人你资料。评论不需要，但是如果你想留评论，可以在下面输入。<br />";
+$l['no_comment'] = "[无评论]";
+$l['vote_added'] = "添加了评分";
+$l['vote_updated'] = "更新评分";
+$l['vote_deleted'] = "删除了评分";
+$l['vote_added_message'] = "你的声望评分成功添加到这个用户。";
+$l['vote_updated_message'] = "你的声望评分成功更新到这个用户。";
+$l['vote_deleted_message'] = "这个声望评分已经从这个用户删除。";
+$l['update_reputation_vote'] = "更新你的评分给{1}";
+$l['positive'] = "正面";
+$l['negative'] = "负面";
+$l['neutral'] = "中立";
+$l['user_comments'] = "你的评论给{1}:";
+$l['add_vote'] = "添加评分";
+$l['update_vote'] = "更新评分";
+$l['delete_vote'] = "删除";
+$l['report_vote'] = "报告";
+$l['power_positive'] = "正面 ({1})";
+$l['power_neutral'] = "中立";
+$l['power_negative'] = "负面 ({1})";
+$l['show_all'] = "显示：所有投票";
+$l['show_positive'] = "显示：正面评分";
+$l['show_neutral'] = "显示：中立评分";
+$l['show_negative'] = "显示：负面评分";
+$l['sort_updated'] = "按最新排序";
+$l['sort_username'] = "按用户名排序";
+$l['last_updated'] = "上次更新{1}";
+$l['postrep_given'] = "评分给<a href=\"{1}\">{2}的帖子</a> {3}<br />";
+$l['postrep_given_nolink'] = "评分给{1}的帖子<br />";
+$l['postrep_given_thread'] = "在<a href=\"{1}\">{2}</a>";
+$l['no_reputation_votes'] = "这个用户目前没有任何声望符合下面的指定的标准。";
+$l['delete_reputation_confirm'] = "你希望删除这个声望评分？";
+$l['delete_reputation_log'] = "删除{1}的声望评分(UID: {2})";
+$l['reputations_disabled_group'] = "对于这个用户组的用户声望系统被禁用了。";
+$l['rate_user'] = "评价用户";
